62d08a629df73ab11661b69d93b082ab0d852c9c
CoWesterosRP <137697170+CoWesterosRP@users.noreply.github.com>
2023-06-25 05:14:18 -0700

    Add files via upload

---
+++ data/houses.js
@@ -0,0 +1,29 @@
+var houses = [{
+		"type": "Feature",
+		"properties": {
+			"seat": "Winterfell",
+			"region": "The North",
+			"house": "Stark",
+			"head": "Jon Stark",
+			"status": "Claimed",
+			"rank": "wfell"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-5.4, 67.4]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"seat": "King's Landing",
+			"region": "The Crownlands",
+			"house": "Targaryen",
+			"head": "Queen Visaera",
+			"status": "Claimed",
+			"rank": "klanding"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [23.95, -50.03]
+		}
+	}];
---
+++ data/markers.js
@@ -0,0 +1,189 @@
+	var klanding_icon = L.icon({
+		iconUrl: 'images/klanding-icon.png',
+		iconSize: [38, 36],
+		iconAnchor: [19, 36],
+		popupAnchor: [0, -36],
+		shadowUrl: 'images/klanding-icon-shadow.png',
+		shadowSize: [51, 23],
+		shadowAnchor: [15, 23]
+	});
+
+	var wfell_icon = L.icon({
+		iconUrl: 'images/wfell-icon.png',
+		iconSize: [36, 25],
+		iconAnchor: [18, 25],
+		popupAnchor: [0, -25],
+		shadowUrl: 'images/wfell-icon-shadow.png',
+		shadowSize: [55, 23],
+		shadowAnchor: [18, 23]
+	});
+
+	var pyke_icon = L.icon({
+		iconUrl: 'images/pyke-icon.png',
+		iconSize: [38, 35],
+		iconAnchor: [19, 35],
+		popupAnchor: [0, -35],
+		shadowUrl: 'images/pyke-icon-shadow.png',
+		shadowSize: [55, 33],
+		shadowAnchor: [19, 33]
+	});
+
+	var crock_icon = L.icon({
+		iconUrl: 'images/crock-icon.png',
+		iconSize: [36, 27],
+		iconAnchor: [18, 27],
+		popupAnchor: [0, -27],
+		shadowUrl: 'images/crock-icon-shadow.png',
+		shadowSize: [56, 30],
+		shadowAnchor: [18, 30]
+	});
+
+	var dstone_icon = L.icon({
+		iconUrl: 'images/dstone-icon.png',
+		iconSize: [35, 34],
+		iconAnchor: [16, 34],
+		popupAnchor: [0, -34],
+		shadowUrl: 'images/dstone-icon-shadow.png',
+		shadowSize: [53, 33],
+		shadowAnchor: [16, 33]
+	});
+
+	var eyrie_icon = L.icon({
+		iconUrl: 'images/eyrie-icon.png',
+		iconSize: [36, 36],
+		iconAnchor: [18, 36],
+		popupAnchor: [0, -36],
+		shadowUrl: 'images/eyrie-icon-shadow.png',
+		shadowSize: [53, 27],
+		shadowAnchor: [18, 27]
+	});
+
+	var gotm_icon = L.icon({
+		iconUrl: 'images/gotm-icon.png',
+		iconSize: [27, 19],
+		iconAnchor: [14, 19],
+		popupAnchor: [0, -19],
+		shadowUrl: 'images/gotm-icon-shadow.png',
+		shadowSize: [51, 23],
+		shadowAnchor: [14, 23]
+	});
+
+	var hgarden_icon = L.icon({
+		iconUrl: 'images/hgarden-icon.png',
+		iconSize: [36, 36],
+		iconAnchor: [18, 36],
+		popupAnchor: [0, -36],
+		shadowUrl: 'images/hgarden-icon-shadow.png',
+		shadowSize: [57, 23],
+		shadowAnchor: [18, 23]
+	});
+
+	var hhal_icon = L.icon({
+		iconUrl: 'images/hhal-icon.png',
+		iconSize: [36, 36],
+		iconAnchor: [18, 36],
+		popupAnchor: [0, -36],
+		shadowUrl: 'images/hhal-icon-shadow.png',
+		shadowSize: [50, 27],
+		shadowAnchor: [18, 27]
+	});
+
+	var otown_icon = L.icon({
+		iconUrl: 'images/otown-icon.png',
+		iconSize: [36, 34],
+		iconAnchor: [18, 34],
+		popupAnchor: [0, -34],
+		shadowUrl: 'images/otown-icon-shadow.png',
+		shadowSize: [56, 30],
+		shadowAnchor: [18, 30]
+	});
+
+	var rrun_icon = L.icon({
+		iconUrl: 'images/rrun-icon.png',
+		iconSize: [37, 35],
+		iconAnchor: [19, 35],
+		popupAnchor: [0, -35],
+		shadowUrl: 'images/rrun-icon-shadow.png',
+		shadowSize: [54, 30],
+		shadowAnchor: [19, 30]
+	});
+
+	var ruins_icon = L.icon({
+		iconUrl: 'images/ruins-icon.png',
+		iconSize: [36, 19],
+		iconAnchor: [18, 19],
+		popupAnchor: [0, -19],
+		shadowUrl: 'images/ruins-icon-shadow.png',
+		shadowSize: [54, 18],
+		shadowAnchor: [18, 18]
+	});
+
+	var send_icon = L.icon({
+		iconUrl: 'images/send-icon.png',
+		iconSize: [36, 34],
+		iconAnchor: [18, 34],
+		popupAnchor: [0, -34],
+		shadowUrl: 'images/send-icon-shadow.png',
+		shadowSize: [53, 30],
+		shadowAnchor: [20, 30]
+	});
+
+	var shall_icon = L.icon({
+		iconUrl: 'images/shall-icon.png',
+		iconSize: [36, 35],
+		iconAnchor: [18, 35],
+		popupAnchor: [0, -35],
+		shadowUrl: 'images/shall-icon-shadow.png',
+		shadowSize: [50, 30],
+		shadowAnchor: [18, 30]
+	});
+
+	var sspear_icon = L.icon({
+		iconUrl: 'images/sspear-icon.png',
+		iconSize: [32, 37],
+		iconAnchor: [16, 37],
+		popupAnchor: [0, -37],
+		shadowUrl: 'images/sspear-icon-shadow.png',
+		shadowSize: [52, 30],
+		shadowAnchor: [18, 30]
+	});
+
+	var twins_icon = L.icon({
+		iconUrl: 'images/twins-icon.png',
+		iconSize: [31, 25],
+		iconAnchor: [16, 25],
+		popupAnchor: [0, -25],
+		shadowUrl: 'images/twins-icon-shadow.png',
+		shadowSize: [45, 23],
+		shadowAnchor: [16, 23]
+	});
+
+	var rank1_icon = L.icon({
+		iconUrl: 'images/rank1-icon.png',
+		iconSize: [19, 21],
+		iconAnchor: [10, 21],
+		popupAnchor: [0, -21],
+		shadowUrl: 'images/rank1-icon-shadow.png',
+		shadowSize: [36, 20],
+		shadowAnchor: [10, 20]
+	});
+
+	var rank2_icon = L.icon({
+		iconUrl: 'images/rank2-icon.png',
+		iconSize: [29, 22],
+		iconAnchor: [15, 22],
+		popupAnchor: [0, -22],
+		shadowUrl: 'images/rank2-icon-shadow.png',
+		shadowSize: [48, 24],
+		shadowAnchor: [15, 24]
+	});
+
+	var rank3_icon = L.icon({
+		iconUrl: 'images/rank3-icon.png',
+		iconSize: [27, 28],
+		iconAnchor: [13, 28],
+		popupAnchor: [0, -28],
+		shadowUrl: 'images/rank3-icon-shadow.png',
+		shadowSize: [47, 22],
+		shadowAnchor: [15, 20]
+	});
